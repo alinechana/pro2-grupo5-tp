5f3101b93d518fefa8935cd4ee1e7f11c6de0e18
alinechana <achana@udesa.edu.ar>
2025-04-02 19:24:04 -0300

    carpeta db con datos estructura

---
+++ myapp/db/datos.js
@@ -0,0 +1,29 @@
+let usuario = {
+    
+
+
+}; 
+
+
+let productos = [
+    producto1 = {}, 
+
+    producto2 = {},
+
+    producto3 = {},
+
+    producto4 = {},
+
+    producto5 = {},
+
+    producto6 = {},
+
+    producto7 = {},
+
+    producto8 = {},
+
+    producto9 = {},
+
+    producto10 = {},
+]
+
