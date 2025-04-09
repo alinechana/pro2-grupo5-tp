72680b581cf8f602f56280dfce9937aec8a1402e
jochuzanatta <jzanatta@udesa.edu.ar>
2025-04-08 21:23:54 -0300

    ruta, controlador y vista de product

--- myapp/controllers/productController.js
+++ myapp/controllers/productController.js
@@ -1,6 +1,18 @@
+const datos = require("../db/datos");
+
 const productController = {
     detalle: function (req, res) {
-        return res.render("product");
+        let productoBuscado = req.params.id;
+        let productoEncontrado = {};
+
+        for (let i = 0; i < datos.productos.length; i++) {
+            const element = datos.productos[i];
+            if (element.id == productoBuscado) {
+                productoEncontrado = element
+            }
+        }
+
+        return res.render("product", {detalle: productoEncontrado, comentarios: datos.comentarios });
         
       },
     agregar: function (req, res) {
--- myapp/routes/product.js
+++ myapp/routes/product.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const productController = require("../controllers/productController");
-
+const datos = require("../db/datos")
 // detalle 
 router.get("/id/:id", productController.detalle);
 
