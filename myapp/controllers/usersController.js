2ceeb11a4a11f258120d7a7bd29a3f948ba05fd4
alinechana <achana@udesa.edu.ar>
2025-05-19 17:59:53 -0300

    registracion en controller, procesar datos de contraseña y email

--- myapp/controllers/usersController.js
+++ myapp/controllers/usersController.js
@@ -1,11 +1,57 @@
 const datos = require("../db/datos");
 const db = require(`../database/models`);
+const bcrypt = require("bcryptjs");
 
 const usersController = {
     register: function (req, res) {
         return res.render("register")
         
       },
+      create: function(req,res){
+        let email = req.body.email 
+
+        db.Usuario.findOne({
+          where: [{ email: email}]
+        })
+        .then(function (resultado) {
+          if (resultado.email == email) {
+            return res.send("Tu mail ya esta registrado")
+            
+          }
+          
+        })
+        .catch(function (error) {
+          return res.send(error)
+          
+        })
+
+        let contrasenia = req.body.contraseña
+        if (contrasenia.length < 3 || constrasenia == " ") {
+          return res.send("La contraseña debe ser mayor a 3 caracteres")
+
+        } else {
+          let passEncriptada = bcrypt.hashSync(contrasenia, 10);
+
+        db.Usuario.create({
+          usuario: req.body.name, 
+          email: req.body.email, 
+          contrasenia: passEncriptada
+        })
+        .then(function (user) {
+          return res.redirect("/")
+        }) 
+        .catch(function (error) {
+          return res.send(error)
+          
+        })
+
+        }
+
+      }, 
+
+
+
+
     login: function (req, res) {
       return res.render("login")
     },
