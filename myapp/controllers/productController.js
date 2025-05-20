5151520dead8d12a1bfa00aee012f200dbc299ac
JustinaNavarro <justinanavarro@udesa.edu.ar>
2025-05-20 19:55:01 -0300

    Search results controlador

--- myapp/controllers/productController.js
+++ myapp/controllers/productController.js
@@ -1,6 +1,7 @@
 const datos = require("../db/datos");
 const db = require(`../database/models`);
 const Producto = db.Producto; 
+const op = db.Sequelize.Op;
 
 const productController = {
     detalle: function (req, res) {
@@ -18,14 +19,33 @@ const productController = {
         return res.render("product", {detalle: productoEncontrado, comentarios: datos.comentarios }); //va a devolver el producto que encontro y los comentarios que corresponden
         
       },
+    
     agregar: function (req, res) {
         return res.render("product-add", {usuario: datos.usuario});
         
     },
+    
     buscar: function (req, res) {
-        return res.render("search-results", {datos});
+        let busqueda = req.query.name; //busca el query del forms
+
+        Producto.findAll({
+            where:{ name: {[Op.like]: '%' + busqueda + '%' }}, //buscar producto que coincida con la busqueda
+            include: [{
+                association: "usuarios" //trae datos del usuario para ver quien publico cada producto buscado
+            }]
+        })
+
+        .then(function (resultados) {
+            if (resultados.length === 0) { //si no hay resultados, 
+                return res.render("search-results", {resultados: [], //se muestra la vista con el mensaje
+                    mensaje: "No hay resultados para la búsqueda"})
+            }
+
+            res.render("search-results", { productos: resultados }); //si hay resultados, se muestran los productos
+            
+        });
         
-    },
+    }
 
     }
 
